ef8e2e9d0e45a8b164b20cab4e44f1813ebe5e6a
gordon <kellymax@126.com>
2020-08-16 15:53:21 +0800

    fixed issue #31

--- src/extension.ts
+++ src/extension.ts
@@ -5,87 +5,112 @@
  *-------------------------------------------------------------*/
 
 import {
-  ConfigurationChangeEvent,
-  ExtensionContext,
-  window,
-  workspace,
-} from 'vscode';
-import { registerViewEvent } from './registerEvent';
-import { FundService } from './service';
-import { isStockTime } from './utils';
-import { FundProvider } from './views/fundProvider';
-import { FundModel } from './views/model';
-import { StatusBar } from './views/statusBar';
-import { StockProvider } from './views/stockProvider';
-
-let intervalTimer: NodeJS.Timer | null = null;
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "leek-fund" is now active!');
-
-  let interval = workspace.getConfiguration().get('leek-fund.interval', 10000);
-
-  if (interval < 3000) {
-    interval = 3000;
-  }
-  // fund
-  const fundService = new FundService(context);
-  const nodeFundProvider = new FundProvider(fundService);
-  nodeFundProvider.refresh();
-  // stock
-  const nodeStockProvider = new StockProvider(fundService);
-  nodeStockProvider.refresh();
-
-  const model = new FundModel();
-  // status bar
-  const statusBar = new StatusBar(fundService);
-
-  // 获取所有基金代码
-  fundService.getFundSuggestList();
-
-  // 第一次主动获取一次数据，因为面板需要点击才触发查询（闭市的时候）
-  fundService.getFundData(model.getCfg('leek-fund.funds'), 0).then(() => {
-    statusBar.refresh();
-  });
-  fundService.getStockData(model.getCfg('leek-fund.stocks'), 0).then(() => {
-    statusBar.refresh();
-  });
-
-  // interval
-  intervalTimer = setInterval(() => {
-    if (isStockTime() || fundService.szItem === undefined) {
-      nodeFundProvider.refresh();
-      nodeStockProvider.refresh();
-      statusBar.refresh();
-    } else {
-      console.log('StockMarket Closed! Polling closed!');
+    ConfigurationChangeEvent,
+    ExtensionContext,
+    window,
+    workspace,
+  } from 'vscode';
+  import { registerViewEvent } from './registerEvent';
+  import { FundService } from './service';
+  import { isStockTime } from './utils';
+  import { FundProvider } from './views/fundProvider';
+  import { FundModel } from './views/model';
+  import { StatusBar } from './views/statusBar';
+  import { StockProvider } from './views/stockProvider';
+  
+  let intervalTimer: NodeJS.Timer | null = null;
+  
+  // this method is called when your extension is activated
+  // your extension is activated the very first time the command is executed
+  export function activate(context: ExtensionContext) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "leek-fund" is now active!');
+  
+    let interval = workspace.getConfiguration().get('leek-fund.interval', 10000);
+  
+    if (interval < 3000) {
+      interval = 3000;
     }
-  }, interval);
-
-  // views
-  window.registerTreeDataProvider('views.fund', nodeFundProvider);
-  window.registerTreeDataProvider('views.stock', nodeStockProvider);
-
-  workspace.onDidChangeConfiguration((e: ConfigurationChangeEvent) => {
-    console.log('配置改变刷新数据');
+    // fund
+    const fundService = new FundService(context);
+    const nodeFundProvider = new FundProvider(fundService);
     nodeFundProvider.refresh();
+    // stock
+    const nodeStockProvider = new StockProvider(fundService);
     nodeStockProvider.refresh();
-    statusBar.refresh();
-  });
+  
+    const model = new FundModel();
+    // status bar
+    const statusBar = new StatusBar(fundService);
+  
+    // 获取所有基金代码
+    fundService.getFundSuggestList();
+  
+    // 第一次主动获取一次数据，因为面板需要点击才触发查询（闭市的时候）
+    //   fundService.getFundData(model.getCfg('leek-fund.funds'), 0).then(() => {
+    //     statusBar.refresh();
+    //   });
+    //   fundService.getStockData(model.getCfg('leek-fund.stocks'), 0).then(() => {
+    //     statusBar.refresh();
+    //   });
+  
+    const tvFund = window.createTreeView('views.fund',{
+      treeDataProvider:nodeFundProvider
+    });
+    const tvStock = window.createTreeView('views.stock',{
+      treeDataProvider:nodeStockProvider
+    });
+    
+    // interval
+    intervalTimer = setInterval(() => {
+      refresh();
+    }, interval);
+  
+    // views
+    //window.registerTreeDataProvider('views.fund', nodeFundProvider);
+    //window.registerTreeDataProvider('views.stock', nodeStockProvider);
+  
+    workspace.onDidChangeConfiguration((e: ConfigurationChangeEvent) => {
+      console.log('配置改变刷新数据');
+      refresh();
+    });
+  
+    // register event
+    registerViewEvent(context, fundService, nodeFundProvider, nodeStockProvider);
+  
+      
+    function refresh() {
+        if (isStockTime() || fundService.szItem === undefined) {
+        
+        if (tvStock.visible) {
+            // 如果侧边栏打开，则直接使用属性更新
+            nodeFundProvider.refresh();
+            nodeStockProvider.refresh();
+            statusBar.refresh();
+        }
+        else{ 
+            // 如果侧边栏未打开，则主动请求数据后更新
+            fundService.getFundData(model.getCfg('leek-fund.funds'), 0).then(() => {
+            statusBar.refresh();
+        });
+            fundService.getStockData(model.getCfg('leek-fund.stocks'), 0).then(() => {
+                statusBar.refresh();
+            });
+        }
 
-  // register event
-  registerViewEvent(context, fundService, nodeFundProvider, nodeStockProvider);
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
-  console.log('deactivate');
-  if (intervalTimer) {
-    clearInterval(intervalTimer);
-    intervalTimer = null;
+        } else {
+            console.log('StockMarket Closed! Polling closed!');
+        }
+    }
   }
-}
+  
+  // this method is called when your extension is deactivated
+  export function deactivate() {
+    console.log('deactivate');
+    if (intervalTimer) {
+      clearInterval(intervalTimer);
+      intervalTimer = null;
+    }
+  }
+  
