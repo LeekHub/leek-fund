6b20b3b67de3a6596a4f19a1dbf8b0bd28a8d073
sungongwei <273965695@qq.com>
2022-01-11 17:28:50 +0800

    style: some semi

--- src/webview/stockTrend.ts
+++ src/webview/stockTrend.ts
@@ -19,17 +19,17 @@ function stockTrend(code: string, name: string, stockCode: string) {
     market = stockCode.substring(0, 2) === 'sh' ? '1' : '0';
   }
   let mcid = market + '.' + code.substr(1);
-  let url = `http://quote.eastmoney.com/basic/full.html?mcid=${mcid}`
+  let url = `http://quote.eastmoney.com/basic/full.html?mcid=${mcid}`;
 
   // TODO：问题1. 需要选择合适的显示页面。上面的 eastmoney 网站不支持期货，market 113在这个网页上不支持。
   // 问题 2. 如果选用东财传统网页，存在交易代码不一致问题。例如甲醇 `MA2201` 在东财上的代码为 `MA201`，`PVC又是 v2201`
   const isFuture = stockCode.indexOf('cnf_') === 0;
   if (isFuture) {
     stockCode = stockCode.replace('cnf_', '').toUpperCase();
-    url = `https://finance.sina.com.cn/futures/quotes/${stockCode}.shtml`
+    url = `https://finance.sina.com.cn/futures/quotes/${stockCode}.shtml`;
   }
   
-  let tabTitle = !isFuture? `股票实时走势(${code})` : `期货实时走势(${name})`
+  let tabTitle = !isFuture? `股票实时走势(${code})` : `期货实时走势(${name})`;
   const panel = ReusedWebviewPanel.create(
     'stockTrendWebview',
     tabTitle,
