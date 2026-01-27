64e782c56ea2d69fe4423fa52663d16749288a3a
giscafer <giscafer@outlook.com>
2026-01-27 22:48:55 +0800

    fix: WebSocket import

--- src/output/flash-news/impl/Jin10FlushService.ts
+++ src/output/flash-news/impl/Jin10FlushService.ts
@@ -1,4 +1,4 @@
-import * as WebSocket from 'ws';
+import WebSocket from 'ws';
 import globalState from '../../../globalState';
 import FlashNewsDaemon from '../FlashNewsDaemon';
 import NewsFlushServiceAbstractClass from '../NewsFlushServiceAbstractClass';
